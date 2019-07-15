8ba1bca8a43bf5f47c46971d01c648b10e3309c9
Afloatz <florentlaot.fr@gmail.com>
2019-07-15 15:08:53 +0200

    wp admin bar

--- wp-content/themes/wpbootstrap/index.php
+++ wp-content/themes/wpbootstrap/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-    <title>Blog Template for Bootstrap</title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title>
+        <?php bloginfo('name'); ?> |
+        <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
+    </title>
     <!-- Bootstrap core CSS -->
     <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <?php wp_head(); ?>
 </head>
 <body>
 <div class="blog-masthead">
@@ -148,8 +150,10 @@
     </p>
 </footer>
 
+<?php wp_footer(); ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="js/bootstrap.js"></script>
+
 </body>
 </html>
